41114d9e82db8364b529768ddac49d728fa7660e
Serigne Toure <serignemortoure1@gmail.com>
2021-03-18 03:01:14 +0000

    finsihed all webchat functionnality

--- webchat/main.js
+++ webchat/main.js
@@ -56,3 +56,87 @@ pc.ontrack = event => {
 };
 
 remoteVideo.srcObject = remoteStream;
+
+callButton.onclick = async() => {
+    // Reference Firestore collections for signaling
+    const callDoc = firestore.collection('calls').doc();
+    const offerCandidates = callDoc.collection('offerCandidates');
+    const answerCandidates = callDoc.collection('answerCandidates');
+
+    callInput.value = callDoc.id;
+
+    // Get candidates for caller, save to db
+    pc.onicecandidate = event => {
+        event.candidate && offerCandidates.add(event.candidate.toJSON());
+    };
+
+    // Create offer
+    const offerDescription = await pc.createOffer();
+    await pc.setLocalDescription(offerDescription);
+
+    const offer = {
+        sdp: offerDescription.sdp,
+        type: offerDescription.type,
+    };
+
+    await callDoc.set({ offer });
+
+    // Listen for remote answer
+    callDoc.onSnapshot((snapshot) => {
+        const data = snapshot.data();
+        if (!pc.currentRemoteDescription && data?.answer) {
+            const answerDescription = new RTCSessionDescription(data.answer);
+            pc.setRemoteDescription(answerDescription);
+        }
+    });
+
+    // Listen for remote ICE candidates
+    answerCandidates.onSnapshot(snapshot => {
+        snapshot.docChanges().forEach((change) => {
+            if (change.type === 'added') {
+                const candidate = new RTCIceCandidate(change.doc.data());
+                pc.addIceCandidate(candidate);
+            }
+        });
+    });
+}
+
+answerButton.onclick = async() => {
+    const callId = callInput.value;
+    const callDoc = firestore.collection('calls').doc(callId);
+    const offerCandidates = callDoc.collection('offerCandidates');
+    const answerCandidates = callDoc.collection('answerCandidates');
+
+    pc.onicecandidate = event => {
+        event.candidate && answerCandidates.add(event.candidate.toJSON());
+    };
+
+    // Fetch data, then set the offer & answer
+
+    const callData = (await callDoc.get()).data();
+
+    const offerDescription = callData.offer;
+    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+
+    const answerDescription = await pc.createAnswer();
+    await pc.setLocalDescription(answerDescription);
+
+    const answer = {
+        type: answerDescription.type,
+        sdp: answerDescription.sdp,
+    };
+
+    await callDoc.update({ answer });
+
+    // Listen to offer candidates
+
+    offerCandidates.onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+            console.log(change)
+            if (change.type === 'added') {
+                let data = change.doc.data();
+                pc.addIceCandidate(new RTCIceCandidate(data));
+            }
+        });
+    });
+};
